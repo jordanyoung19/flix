912869c8711665f6e562bbf1a7470291329e4891
Jordan Young <jordanby@uci.edu>
2020-04-11 17:52:29 -0700

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Jordan Young on 4/11/20.
+//  Copyright © 2020 Jordan Young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
